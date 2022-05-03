460412c1b32e93b4333063ae5bf3cff100893845
Srishti <75185963+Srishti-Kumar@users.noreply.github.com>
2022-05-04 03:12:51 +0530

    Max & min of 3
    Define two methods to print the maximum and the minimum number respectively among three numbers entered by the user.

--- 1.function.md/1maxMin.java
+++ 1.function.md/1maxMin.java
@@ -1,1 +1,60 @@
+package com.functions;
+
+import java.util.Scanner;
+
+public class maxMin {
+    public static void main(String[] args) {
+        System.out.print("Enter a,b,c: ");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        System.out.println("Max of 3 is: " + max(a,b,c));
+        System.out.println("Min of 3 is: " + min(a,b,c));
+        //max(a,b,c);
+        //min(a,b,c);
+    }
+
+    public static int max(int a,int b,int c) {
+        if(a>b){
+            if(a>c){
+                return a;
+            }
+
+            else{
+                return c;
+            }
+
+        }
+        else{
+            if(b>c){
+                return b;
+            }
+            else {
+                return c;
+            }
+        }
+    }
+
+    public static int min(int a,int b,int c){
+        if(a<b){
+            if(a<c){
+                return a;
+            }
+
+            else{
+                return c;
+            }
+
+        }
+        else{
+            if(b<c){
+                return b;
+            }
+            else {
+                return c;
+            }
+        }
+    }
+}
 
