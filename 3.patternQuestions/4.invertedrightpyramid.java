df337e5923b6df770f14f15c7de00659363149ee
Srishti <75185963+Srishti-Kumar@users.noreply.github.com>
2022-09-14 17:49:30 +0530

    4

---
+++ 3.patternQuestions/4.invertedrightpyramid.java
@@ -0,0 +1,24 @@
+package com.PatternQuestions;
+
+public class invertedrightpyramid {
+    public static void main(String[] args) {
+        int row = 5;
+        int column = 5;
+
+        for(int i=0;i<row;i++){
+            for(int j=0;j<column;j++){
+                if(i>j)
+                    System.out.print("  ");
+                else
+                    System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+//    * * * * *
+//      * * * *
+//        * * *
+//          * *
+//            *
+}
