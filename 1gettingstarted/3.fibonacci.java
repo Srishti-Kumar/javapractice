a0cfe92aa08e0911f7cf050a8258ec5ad76da21c
Srishti <75185963+Srishti-Kumar@users.noreply.github.com>
2022-09-17 13:09:44 +0530

    3 fibonacci

--- 1gettingstarted/3.fibonacci.java
+++ 1gettingstarted/3.fibonacci.java
@@ -4,14 +4,16 @@ import java.util.*;
   
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    long num = sc.nextLong();
-
-    int digits =0;
-    while(num != 0){
-      num = num / 10;
-      digits++;
+    int n = sc.nextInt();
+    
+    int a = 0;
+    int b = 1;
+    for(int i=0;i<n;i++){
+      System.out.println(a);
+      int c = a+b;
+      a = b;
+      b = c;      
     }
-    System.out.println(digits);
 
   }
 }
